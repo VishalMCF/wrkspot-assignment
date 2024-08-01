47e5c8867abbc3be18c4184fba9799895b72c792
Vishal M Yadav <vishal.yadav@embibe.com>
2024-08-01 21:15:51 +0530

    Initial commit

---
+++ src/main/java/com/example/asswrkspt/AsswrksptApplication.java
@@ -0,0 +1,13 @@
+package com.example.asswrkspt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsswrksptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsswrksptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asswrkspt/AsswrksptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asswrkspt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsswrksptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
